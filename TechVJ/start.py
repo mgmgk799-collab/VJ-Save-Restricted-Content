0047b1b10f352cfb2d1d7a6df905c4cfa1dc5975
mgmgk799-collab <mgmgk799@gmail.com>
2026-06-27 12:46:17 +0630

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -5,6 +5,7 @@
 import os
 import asyncio 
 import pyrogram
+import random
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated, UserAlreadyParticipant, InviteHashExpired, UsernameNotOccupied
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message 
@@ -183,7 +184,8 @@ async def save(client: Client, message: Message):
                         if ERROR_MESSAGE == True:
                             await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
 
-            # wait time
+            custom_wait = random.randint(30, 90)
+            await asyncio.sleep(custom_wait)
             await asyncio.sleep(WAITING_TIME)
         if LOGIN_SYSTEM == True:
             try:
@@ -226,11 +228,11 @@ async def handle_private(client: Client, acc, message: Message, chatid: int, msg
             await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id, parse_mode=enums.ParseMode.HTML) 
         return await smsg.delete()
     if batch_temp.IS_BATCH.get(message.from_user.id): return 
+		upload_delay = random.randint(5, 15)
+    await asyncio.sleep(upload_delay)
     asyncio.create_task(upstatus(client, f'{message.id}upstatus.txt', smsg, chat))
 
-    if msg.caption:
-        caption = msg.caption
-    else:
+ 
         caption = None
     if batch_temp.IS_BATCH.get(message.from_user.id): return 
             
